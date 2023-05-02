1eebfcf3e7fe70d702e5b533661c486a2c20c3da
jrfnl <jrfnl@users.noreply.github.com>
2023-05-02 23:54:25 +0200

    Remove valid `@covers` tags, leaving only invalid - coverage 80% instead of expected 0%
    CLI output:
    ```
    1 test triggered 1 PHPUnit warning:
    
    1) Jrf\CodeCoverage\Scenario\Tests\FooTest::testMethodAB
    "@covers \Jrf\CodeCoverage\Scenario\Foo::methodDoesNotExist" is invalid
    
    D:\000_GitHub\_TEMP\bug-report-reproduction-scenarios\tests\FooTest.php:14
    
    WARNINGS!
    Tests: 1, Assertions: 1, Warnings: 1.
    
    Generating code coverage report in Clover XML format ... done [00:00.001]
    
    Generating code coverage report in HTML format ... done [00:00.020]
    
    Code Coverage Report Summary:
      Classes:  0.00% (0/1)
      Methods: 80.00% (4/5)
      Paths:   80.00% (4/5)
      Branches:   80.00% (4/5)
      Lines:   80.00% (4/5)
    ```

--- tests/FooTest.php
+++ tests/FooTest.php
@@ -9,8 +9,6 @@ class FooTest extends TestCase
 {
 
     /**
-     * @covers \Jrf\CodeCoverage\Scenario\Foo::methodA
-     * @covers \Jrf\CodeCoverage\Scenario\Foo::methodB
      * @covers \Jrf\CodeCoverage\Scenario\Foo::methodDoesNotExist
      */
     public function testMethodAB() {
